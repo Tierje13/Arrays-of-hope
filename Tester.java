33e22bf6b3653a8100ac85e4f1554abf7d1fca07
Tierje13 <tierje11@gmail.com>
2016-11-16 13:19:55 -0700

    Final

---
+++ Tester.java
@@ -0,0 +1,20 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		char ch[] = new char[900];
+		int j;
+		for (j=65; j<=90; j++){
+			ch[j]=(char)j;
+			if (j==90)
+				System.out.print(ch[j]);
+			else {
+			System.out.print(ch[j]+", ");
+			}
+			
+		
+
+	}
+		
+	}
+}
